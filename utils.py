584819c3e8b2a25b5b4c2fd5de60a081ed5fd00e
SunLu Z <40049927+zsl24@users.noreply.github.com>
2022-01-14 11:47:02 +0800

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,12 @@
+import numpy as np
+from transformers import EvalPrediction
+
+is_regression = False
+def compute_metrics(p: EvalPrediction):
+    preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
+    preds = np.squeeze(preds) if is_regression else np.argmax(preds, axis=1)
+
+    if is_regression:
+        return {"mse": ((preds - p.label_ids) ** 2).mean().item()}
+    else:
+        return {"accuracy": (preds == p.label_ids).astype(np.float32).mean().item()}
